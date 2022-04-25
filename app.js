3b606a3d204e176c23958f2adeb835e1abb0b3ef
Aarti-Jain <aartijain@brandeis.edu>
2022-04-25 13:21:18 -0400

    added bash shell to put mongodb enviornment in deleted .env file

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const user = require("./models/User")
 
 /* initialize the database*/
 const mongoose = require('mongoose');
-require('dotenv').config();
+//require('dotenv').config();
 mongoose.connect( process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
 
 const db = mongoose.connection; //connects to the database
---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export MONGO_URI='mongodb+srv://aartijain:liberty312001@cluster0.wzkxj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+echo "connecting to $mongodb_URI"
+nodemon
