2a2ec029e38ee5fe123638c5edd2bea21d21dab8
Aarti-Jain <aartijain@brandeis.edu>
2022-04-25 13:27:58 -0400

    minor change

--- startup.sh
+++ startup.sh
@@ -2,4 +2,3 @@
 export MONGO_URI='mongodb+srv://aartijain:liberty312001@cluster0.wzkxj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 echo "connecting to $mongodb_URI"
 npm install 
-node app.js
